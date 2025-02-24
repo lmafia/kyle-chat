d08ec3dfb699a0bdc321b450a0cda453f654801a
lmafia <lmafiamm@gmail.com>
2025-02-25 00:46:30 +0800

    Initial Commit

---
+++ kyle-chat/kyle_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  kyle_chatApp.swift
+//  kyle-chat
+//
+//  Created by lmafia on 2025/2/25.
+//
+
+import SwiftUI
+
+@main
+struct kyle_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kyle-chatTests/kyle_chatTests.swift
@@ -0,0 +1,17 @@
+//
+//  kyle_chatTests.swift
+//  kyle-chatTests
+//
+//  Created by lmafia on 2025/2/25.
+//
+
+import Testing
+@testable import kyle_chat
+
+struct kyle_chatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
